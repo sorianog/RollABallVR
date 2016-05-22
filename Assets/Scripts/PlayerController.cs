41d5139ef374881637d26141b5f2f43354f85af3
Gerald Soriano <sorianog@msoe.edu>
2016-05-22 14:11:15 -0500

    Completed Part 3/8 of video tutorial. The player sphere can now move.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float speed;
+    private Rigidbody rigBod;
+
+    void Start()
+    {
+        rigBod = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Called before rendering a frame 
+    /// </summary>
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Called before performing any physics calculations 
+    /// </summary>
+    void FixedUpdate()
+    {
+        float moveHort = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHort, 0.0f, moveVert);
+
+        rigBod.AddForce(movement * speed);
+    }
 }
