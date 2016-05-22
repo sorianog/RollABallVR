9b9e41b677d4f658d2eda893947d7ec7d516136b
Gerald Soriano <sorianog@msoe.edu>
2016-05-22 15:58:57 -0500

    Completed game logic and added UI elements.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
     private Rigidbody rigBod;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     void Start()
     {
         rigBod = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     /// <summary>
-    /// Called once per frame 
+    /// Update is called once per frame 
     /// </summary>
     void Update()
     {
@@ -30,5 +38,24 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHort, 0.0f, moveVert);
 
         rigBod.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 14)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
